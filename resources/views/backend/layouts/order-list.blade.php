28c58d5dd9a1a6681f37846d3ef6a8111ff511f6
foysal11203004 <foysal11203004@gmail.com>
2021-03-16 17:08:42 +0600

    blade templating

---
+++ resources/views/backend/layouts/order-list.blade.php
@@ -0,0 +1,7 @@
+@extends('backend.master')
+
+
+@section('content')
+
+    <h1>Hello order</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Backend\OrderController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,9 +10,11 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+|'
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',function (){
+   return view('backend.master');
 });
+Route::get('/orders/list',[OrderController::class,'list'])->name('order.list');
+
